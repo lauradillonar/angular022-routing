f2e8c2f4df4aadc175be1bb798fc196431cab016
lauradillonar <laura.dillon.ar@gmail.com>
2022-04-15 11:02:56 -0300

    Añadiendo rutas dentro del array de Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { AmazonComponent } from './components/amazon/amazon.component';
+import { FacebookComponent } from './components/facebook/facebook.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GoogleComponent } from './components/google/google.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'google', component: GoogleComponent},
+  {path: 'facebook', component: FacebookComponent},
+  {path: 'amazon', component: AmazonComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
